359af51902fa686cdfacc61e9fd0b0dddb7b7106
Meaghan Campbell <emeaghancampbell@gmail.com>
2021-01-28 17:36:16 -0600

    updated application

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,64 +5,76 @@ var searchBtnEl = document.querySelector('#search-btn')
 var searchInputEl = document.querySelector('#search-input')
 
 // select player container
-var playerSectionEl = document.querySelector('#song-display')
+var artistSectionEl = document.querySelector('#similar-artist-display')
+
+// select artist name display
+var artistNameDisplay = document.querySelector('#artist-name')
 
 var submitSearch = function(event) {
     event.preventDefault();
     
     // get value from city name input
-    var songTitle = searchInputEl.value.trim();
+    var artistName = searchInputEl.value.trim();
 
-    if (songTitle) {
-        displaySongPlayer(songTitle);
+    if (artistName) {
+        displaySongPlayer(artistName);
         searchInputEl.value = "";
     } else {
-        alert('Please enter a song title')
+        alert('Please enter a valid artist name.')
     }
 }
 
 // function to fetch spotify data here
 
 // function to display player
-var displaySongPlayer = function(songTitle) {
+var displaySongPlayer = function(artistName) {
 
+    artistNameDisplay.textContent = artistName
+
+    for (var i = 0; i < 5; i++) {
     // create player container (ul)
-    var playerContainerEl = document.createElement('ul')
-    playerContainerEl.classList.add('player-container')
+    var artistContainerEl = document.createElement('ul')
+    artistContainerEl.classList.add('artist-container')
 
     // create song background (li)
-    var songBackgroundEl = document.createElement('li')
-    songBackgroundEl.className = 'song-background'
+    var artistBackgroundEl = document.createElement('li')
+    artistBackgroundEl.className = 'artist-background'
 
     // create p element and give it value of songTitle (searched term)
-    var songTitleEl = document.createElement('p')
-    songTitleEl.classList.add('song-details')
-    songTitleEl.textContent = songTitle
+    var artistNameEl = document.createElement('p')
+    artistNameEl.classList.add('song-details')
+    artistNameEl.textContent = 'similar artist name' // this will be data...[i] to loop through and display 5 similar artists
 
     // create container for buttons
     var btnContainerEl = document.createElement('div')
     btnContainerEl.classList.add('button-container')
 
     // create learn button
-    var learnBtnEl = document.createElement('button')
-    learnBtnEl.classList.add('learn')
-    learnBtnEl.textContent = 'Learn to Play!'
+    var showsBtnEl = document.createElement('button')
+    showsBtnEl.classList.add('shows')
+    showsBtnEl.textContent = 'Find Shows'
 
     // create trash button and icon
     var trashBtnEl = document.createElement('button')
     trashBtnEl.classList.add('trash')
     trashBtnEl.classList.add('far')
     trashBtnEl.classList.add('fa-trash-alt')
+    // id for trash button so we can target to remove item
+    trashBtnEl.setAttribute('id', 'trash-btn')
 
     // append elements to page
-    playerSectionEl.appendChild(playerContainerEl)
-    playerContainerEl.appendChild(songBackgroundEl)
-    songBackgroundEl.appendChild(songTitleEl)
-    songBackgroundEl.appendChild(btnContainerEl)
-    btnContainerEl.appendChild(learnBtnEl)
+    artistSectionEl.appendChild(artistContainerEl)
+    artistContainerEl.appendChild(artistBackgroundEl)
+    artistBackgroundEl.appendChild(artistNameEl)
+    artistBackgroundEl.appendChild(btnContainerEl)
+    btnContainerEl.appendChild(showsBtnEl)
     btnContainerEl.appendChild(trashBtnEl)
+
+    }
 
 }
 
+
 // listen for search button click
 searchBtnEl.addEventListener('click', submitSearch);
+
