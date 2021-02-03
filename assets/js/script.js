e1a0d05c9ee1d20ebd3751846b7a61bf30615073
tpgent01 <73126336+tpgent01@users.noreply.github.com>
2021-02-03 13:08:31 -0600

    js modal code added

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,7 +23,46 @@ var searchRecentFunction = function () {
 }
 }
 
+// Modal
+var openmodal = document.querySelectorAll('.modal-open')
+for (var i = 0; i < openmodal.length; i++) {
+  openmodal[i].addEventListener('click', function(event){
+  event.preventDefault()
+  toggleModal()
+  })
+}
 
+const overlay = document.querySelector('.modal-overlay')
+overlay.addEventListener('click', toggleModal)
+
+var closemodal = document.querySelectorAll('.modal-close')
+for (var i = 0; i < closemodal.length; i++) {
+  closemodal[i].addEventListener('click', toggleModal)
+}
+
+document.onkeydown = function(evt) {
+  evt = evt || window.event
+  var isEscape = false
+  if ("key" in evt) {
+  isEscape = (evt.key === "Escape" || evt.key === "Esc")
+  } else {
+  isEscape = (evt.keyCode === 27)
+  }
+  if (isEscape && document.body.classList.contains('modal-active')) {
+  toggleModal()
+  }
+};
+
+function toggleModal () {
+  const body = document.querySelector('body')
+  const modal = document.querySelector('.modal')
+  modal.classList.toggle('opacity-0')
+  modal.classList.toggle('pointer-events-none')
+  body.classList.toggle('modal-active')
+}
+
+
+// search function
 var submitSearch = function(event) {
     event.preventDefault();
     artistContainerEl.innerHTML = '';
@@ -37,9 +76,10 @@ var submitSearch = function(event) {
         
         // store input to local storage
         localStorage.setItem("storedArtist", artistName);
-    } else {
-        alert('Please enter a valid artist name.')
-    }  
+    } 
+    //else {
+        //alert('Please enter a valid artist name.')
+    //}  
 }
 
 // search by enter btn
