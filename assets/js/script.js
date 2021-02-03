cb2718261f8be9b354b6a36bacbfa04294cf2137
tpgent01 <73126336+tpgent01@users.noreply.github.com>
2021-02-03 13:10:10 -0600

    moved modal code into if else - not working correctly

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,44 +23,6 @@ var searchRecentFunction = function () {
 }
 }
 
-// Modal
-var openmodal = document.querySelectorAll('.modal-open')
-for (var i = 0; i < openmodal.length; i++) {
-  openmodal[i].addEventListener('click', function(event){
-  event.preventDefault()
-  toggleModal()
-  })
-}
-
-const overlay = document.querySelector('.modal-overlay')
-overlay.addEventListener('click', toggleModal)
-
-var closemodal = document.querySelectorAll('.modal-close')
-for (var i = 0; i < closemodal.length; i++) {
-  closemodal[i].addEventListener('click', toggleModal)
-}
-
-document.onkeydown = function(evt) {
-  evt = evt || window.event
-  var isEscape = false
-  if ("key" in evt) {
-  isEscape = (evt.key === "Escape" || evt.key === "Esc")
-  } else {
-  isEscape = (evt.keyCode === 27)
-  }
-  if (isEscape && document.body.classList.contains('modal-active')) {
-  toggleModal()
-  }
-};
-
-function toggleModal () {
-  const body = document.querySelector('body')
-  const modal = document.querySelector('.modal')
-  modal.classList.toggle('opacity-0')
-  modal.classList.toggle('pointer-events-none')
-  body.classList.toggle('modal-active')
-}
-
 
 // search function
 var submitSearch = function(event) {
@@ -77,10 +39,47 @@ var submitSearch = function(event) {
         // store input to local storage
         localStorage.setItem("storedArtist", artistName);
     } 
-    //else {
-        //alert('Please enter a valid artist name.')
-    //}  
+    else {
+        // Modal
+        var openmodal = document.querySelectorAll('.modal-open')
+        for (var i = 0; i < openmodal.length; i++) {
+          openmodal[i].addEventListener('click', function(event){
+          event.preventDefault()
+          toggleModal()
+          })
+        }
+
+        const overlay = document.querySelector('.modal-overlay')
+        overlay.addEventListener('click', toggleModal)
+
+        var closemodal = document.querySelectorAll('.modal-close')
+        for (var i = 0; i < closemodal.length; i++) {
+          closemodal[i].addEventListener('click', toggleModal)
+        }
+
+        document.onkeydown = function(evt) {
+          evt = evt || window.event
+          var isEscape = false
+          if ("key" in evt) {
+          isEscape = (evt.key === "Escape" || evt.key === "Esc")
+          } else {
+          isEscape = (evt.keyCode === 27)
+          }
+          if (isEscape && document.body.classList.contains('modal-active')) {
+          toggleModal()
+          }
+        };
+
+        function toggleModal () {
+          const body = document.querySelector('body')
+          const modal = document.querySelector('.modal')
+          modal.classList.toggle('opacity-0')
+          modal.classList.toggle('pointer-events-none')
+          body.classList.toggle('modal-active')
+        }
+    }  
 }
+
 
 // search by enter btn
 var pressEnter = document.getElementById('search-input');
